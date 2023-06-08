afed1d1bf2a742760c65189232fdc75d76575983
Yamashita Itsuki <130331086+dojo-NIitsuki@users.noreply.github.com>
2023-06-08 16:52:17 +0900

    F2project

---
+++ F2workspace/f2project/src/servlet/confirmServlet.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class confirmServlet
+ */
+@WebServlet("/confirmServlet")
+public class confirmServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("ID");
+		String pw = request.getParameter("PW");
+		String question = request.getParameter("QUESTION");
+		String answer = request.getParameter("ANSWER");
+
+		if(question.equals("1")) {
+			question="しつもん1";
+		}
+		if(question.equals("2")) {
+			question="しつもん2";
+		}
+		if(question.equals("3")) {
+			question="しつもん3";
+		}
+
+		request.setAttribute("check_id", id);
+		request.setAttribute("check_pw", pw);
+		request.setAttribute("check_question", question);
+		request.setAttribute("check_answer", answer);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/confirm.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
