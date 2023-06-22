2177c684f080dac167fdfc5d34c593196e00f493
dojo2023-tsuji <130334496+dojo2023-tsuji@users.noreply.github.com>
2023-06-22 18:04:25 +0900

    0622_本日の更新部分一部省略版

--- F2workspace/f2project/src/servlet/listServlet.java
+++ F2workspace/f2project/src/servlet/listServlet.java
@@ -26,32 +26,28 @@ public class listServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		SpotDAO sDao = new SpotDAO();
-		List<Spot> cardList = sDao.select(new Spot("","","","","",""));
+		List<Spot> cardList = sDao.select(new Spot("","","","",""));
 		// 検索結果をリクエストスコープに格納する
 		request.setAttribute("cardList", cardList);
 
+		String img_data_x = request.getParameter("img_data_x");
+		String img_data_y = request.getParameter("img_data_y");
+
+		request.setAttribute("img_data_x", img_data_x);
+		request.setAttribute("img_data_y", img_data_y);
+
 // メニューページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/list.jsp");
-		dispatcher.forward(request, response);
+RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/list.jsp");
+dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		String date = request.getParameter("date");
-		String genre = request.getParameter("genre");
-		String spotname = request.getParameter("spotname");
-		String spotaddress = request.getParameter("spotaddress");
-
-		SpotDAO sDao = new SpotDAO();
-		List<Spot> cardList = sDao.select(new Spot("",date,genre,spotname,spotaddress,""));
-		request.setAttribute("cardList", cardList);
-
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/list.jsp");
-		dispatcher.forward(request, response);
-	}
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
 
 }
 
